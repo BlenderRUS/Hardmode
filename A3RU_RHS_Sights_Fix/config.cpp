4c16e54d2acd4f90fa4eacca735216a1cdfc9026
BlenderRUS <sub7blender@gmail.com>
2015-04-24 23:56:27 +0300

    RHS BMD-2* & RDS T-55 Sight fixes

--- A3RU_RHS_Sights_Fix/config.cpp
+++ A3RU_RHS_Sights_Fix/config.cpp
@@ -6,15 +6,16 @@ class CfgPatches
 		weapons[] = {};
 		magazines[] = {};
 		requiredVersion = 1.36;
-		requiredAddons[] = {"rhs_c_t72", "rhs_optics"};
-		version = "1.0";
-		versionStr = "1.0";
-		versionAr[] = {1,0};
+		requiredAddons[] = {"rhs_optics", "RDS_Tanks"};
+		version = "1.1";
+		versionStr = "1.1";
+		versionAr[] = {1,1};
 		author[] = { "Blender" };
 		authorUrl = "http://www.arma3.ru";
-		description = "T72, T80, BTR-80a, BMD2K Sight fixes";
+		description = "BMD2 & RDS T-55 Sight fixes";
 	};
 };
+
 class CfgFunctions
 {
 	class RHS
@@ -22,30 +23,22 @@ class CfgFunctions
 		tag = "RHS";
 		class Functions
 		{
-			class rF_t72
-			{
-				file = "\A3RU_RHS_Sights_Fix\Functions\RHS_rF_t72.sqf";
-				description = "FCS for T72";
-			};
-			class sight_1g42
-			{
-				file = "\A3RU_RHS_Sights_Fix\Functions\RHS_sight_1G42.sqf";
-				description = "FCS for BMP-2";
-			};
-			class sight_1g46
-			{
-				file = "\A3RU_RHS_Sights_Fix\Functions\RHS_sight_1G46.sqf";
-				description = "FCS for BMP-2";
-			};
-			class sight_agava2
-			{
-				file = "\A3RU_RHS_Sights_Fix\Functions\RHS_sight_agava2.sqf";
-				description = "FCS for BMP-2";
-			};
 			class sight_bmp2
 			{
 				file = "\A3RU_RHS_Sights_Fix\Functions\RHS_sight_bmp2.sqf";
 				description = "FCS for BMP-2";
+			};
+		};
+	};
+	class RDS
+	{
+		tag = "RDS";
+		class functions
+		{
+			class sight_t55 
+			{
+				file = "\A3RU_RHS_Sights_Fix\Functions\RDS_sight_T55.sqf";
+				description = "FCS for T55";
 			};
 		};
 	};
