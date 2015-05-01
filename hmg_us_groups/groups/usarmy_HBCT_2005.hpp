b31aa8c265422e2b5261efaef47db65ec41d6596
Wieking <alex.antonov@mail.ru>
2015-05-02 01:49:10 +0400

    косяк Викинга

--- hmg_us_groups/groups/usarmy_HBCT_2005.hpp
+++ hmg_us_groups/groups/usarmy_HBCT_2005.hpp
@@ -1,124 +1,124 @@
-			class group_nato_usarmy_w_infantry_HBCT
+			class group_nato_usarmy_d_infantry_HBCT
 			{
-				name = "US ARMY (HBCT) wood";
-				aliveCategory="Infantry (HBCT) wood";
-				class group_nato_usarmy_w_infantry_HQ
+				name = "US ARMY (HBCT) desert";
+				aliveCategory="Infantry (HBCT) desert";
+				class group_nato_usarmy_d_infantry_HQ
 				{
 					name = "COMPANY HQ";
-					faction = "rhs_faction_usarmy_w_hmg";
+					faction = "rhs_faction_usarmy_d_hmg";
 					side = 1;
 					rarityGroup = 0.75;
 
 					class Unit0
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_hq_comm";
+						vehicle = "us_army_des_ucp_hq_comm";
 						rank = "CAPTAIN";
 						position[] = {0,1,0};
 					};
 					class Unit1
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_hq_execof";
+						vehicle = "us_army_des_ucp_hq_execof";
 						rank = "LIEUTENANT";
 						position[] = {2,0,0 };
 					};
 					class Unit2
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_hq_ifv_mg";
+						vehicle = "us_army_des_ucp_hq_ifv_mg";
 						rank = "SERGEANT";
 						position[] = {3,0,0};
 					};
 					class Unit3
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_hq_ifv_dr";
+						vehicle = "us_army_des_ucp_hq_ifv_dr";
 						rank = "CORPORAL";
 						position[] = {4,0,0};
 					};
 					class Unit4
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_sup_sgt";
+						vehicle = "us_army_des_ucp_sup_sgt";
 						rank = "SERGEANT";
 						position[] = {5,0,0};
 					};
 					class Unit5
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_sup_sp";
+						vehicle = "us_army_des_ucp_sup_sp";
 						rank = "CORPORAL";
 						position[] = {6,0,0};
 					};
 					class Unit6
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_bpla";
+						vehicle = "us_army_des_ucp_bpla";
 						rank = "SERGEANT";
 						position[] = {7,0,0};
 					};
 					class Unit7
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_cp_ratelo";
+						vehicle = "us_army_des_ucp_cp_ratelo";
 						rank = "SERGEANT";
 						position[] = {8,0,0};
 					};
-						class Unit8
+					class Unit8
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_cp_fs_comm";
+						vehicle = "us_army_des_ucp_cp_fs_comm";
 						rank = "SERGEANT";
 						position[] = {9,0,0};
 					};
 					class Unit9
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_cp_fs_dr";
+						vehicle = "us_army_des_ucp_cp_fs_dr";
 						rank = "SERGEANT";
 						position[] = {10,0,0};
 					};
 				};
-				class group_nato_usarmy_w_infantry_PL_HQ
+				class group_nato_usarmy_d_infantry_PL_HQ
 				{
 					name = "RIFLE PLATOON HQ";
-					faction = "rhs_faction_usarmy_w_hmg";
+					faction = "rhs_faction_usarmy_d_hmg";
 					side = 1;
 					rarityGroup = 0.75;
 
 					class Unit0
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_pl_ld";
+						vehicle = "us_army_des_ucp_pl_ld";
 						rank = "LIEUTENANT";
 						position[] = {0,1,0};
 					};
 					class Unit1
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_pl_ratelo";
+						vehicle = "us_army_des_ucp_pl_ratelo";
 						rank = "PRIVATE";
 						position[] = {2,0,0};
 					};
 				};
-				class group_nato_usarmy_w_infantry_VEH_SEC
+				class group_nato_usarmy_d_infantry_VEH_SEC
 				{
 					name = "VEHICLE SECTION";
-					faction = "rhs_faction_usarmy_w_hmg";
+					faction = "rhs_faction_usarmy_d_hmg";
 					side = 1;
 					rarityGroup = 0.75;
 					class Unit0
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_pl_comm";
+						vehicle = "us_army_des_ucp_ifv_pl_comm";
 						rank = "SERGEANT";
 						position[] = {0,1,0 };
 					};
 					class Unit1
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_g";
+						vehicle = "us_army_des_ucp_ifv_g";
 						rank = "SERGEANT";
 						position[] = {2,0,0 };
 					};
@@ -126,91 +126,91 @@
 					class Unit2
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_dr";
+						vehicle = "us_army_des_ucp_ifv_dr";
 						rank = "CORPORAL";
 						position[] = {3,0,0};
 					};
 					class Unit3
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_g";
+						vehicle = "us_army_des_ucp_ifv_g";
 						rank = "SERGEANT";
 						position[] = {4,0,0};
 					};
 					class Unit4
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_dr";
+						vehicle = "us_army_des_ucp_ifv_dr";
 						rank = "CORPORAL";
 						position[] = {5,0,0};
 					};
 					class Unit5
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_g";
+						vehicle = "us_army_des_ucp_ifv_g";
 						rank = "SERGEANT";
 						position[] = {6,0,0};
 					};
 					class Unit6
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_sl";
+						vehicle = "us_army_des_ucp_ifv_sl";
 						rank = "SERGEANT";
-						position[] = {7,9,0};
+						position[] = {7,5,0};
 					};
 					class Unit7
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_dr";
+						vehicle = "us_army_des_ucp_ifv_dr";
 						rank = "CORPORAL";
 						position[] = {8,0,0};
 					};
 					class Unit8
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_g";
+						vehicle = "us_army_des_ucp_ifv_g";
 						rank = "SERGEANT";
 						position[] = {9,0,0};
 					};
 					class Unit9
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_sl";
+						vehicle = "us_army_des_ucp_ifv_sl";
 						rank = "SERGEANT";
-						position[] = {10,9,0};
+						position[] = {10,0,0};
 					};
 					class Unit10
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_dr";
+						vehicle = "us_army_des_ucp_ifv_dr";
 						rank = "CORPORAL";
 						position[] = {11,0,0};
 					};
 					class Unit11
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ifv_g";
+						vehicle = "us_army_des_ucp_ifv_g";
 						rank = "SERGEANT";
 						position[] = {12,0,0};
 					};
 				};
-				class group_nato_usarmy_w_infantry_RIF_SQ
+				class group_nato_usarmy_d_infantry_RIF_SQ
 				{
 					name = "RIFLE SQUAD";
-					faction = "rhs_faction_usarmy_w_hmg";
+					faction = "rhs_faction_usarmy_d_hmg";
 					side = 1;
 					rarityGroup = 0.75;
 					class Unit0
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_sqld";
+						vehicle = "us_army_des_ucp_sqld";
 						rank = "SERGEANT";
-						position[] = {0,1,0};
+						position[] = {0,1,0 };
 					};
 					class Unit1
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_tld";
+						vehicle = "us_army_des_ucp_tld";
 						rank = "CORPORAL";
 						position[] = {2,0,0 };
 					};
@@ -218,49 +218,49 @@
 					class Unit2
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ar";
+						vehicle = "us_army_des_ucp_ar";
 						rank = "CORPORAL";
 						position[] = {3,0,0};
 					};
 					class Unit3
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_gp";
+						vehicle = "us_army_des_ucp_gp";
 						rank = "CORPORAL";
 						position[] = {4,0,0};
 					};
 					class Unit4
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_aarsp";
+						vehicle = "us_army_des_ucp_aarsp";
 						rank = "PRIVATE";
 						position[] = {5,0,0};
 					};
 					class Unit5
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_tld2";
+						vehicle = "us_army_des_ucp_tld2";
 						rank = "CORPORAL";
 						position[] = {6,0,0};
 					};
 					class Unit6
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_ar";
+						vehicle = "us_army_des_ucp_ar";
 						rank = "CORPORAL";
 						position[] = {7,0,0};
 					};
 					class Unit7
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_gp";
+						vehicle = "us_army_des_ucp_gp";
 						rank = "CORPORAL";
 						position[] = {8,0,0};
 					};
 					class Unit8
 					{
 						side = 1;
-						vehicle = "us_army_wood_ocp_marksman";
+						vehicle = "us_army_des_ucp_marksman";
 						rank = "PRIVATE";
 						position[] = {9,0,0};
 					};
