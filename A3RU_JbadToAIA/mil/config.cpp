2a4bc38b467e016e34e769d19bc221e1134d4a28
BlenderRUS <sub7blender@gmail.com>
2015-05-11 20:40:34 +0300

    JbadToAIA: Config syntax FIX

--- A3RU_JbadToAIA/mil/config.cpp
+++ A3RU_JbadToAIA/mil/config.cpp
@@ -25,12 +25,28 @@ class CfgVehicles
 	{
 		class DestructionEffects;
 	};
-	class Land_Mil_Barracks_EP1: House_F { #include "Land_Jbad_Mil_Barracks.hpp" };
-	class Land_Mil_Barracks: House_F { #include "Land_Jbad_Mil_Barracks.hpp" };
-	class Land_Mil_ControlTower_EP1: House_F { #include "Land_Jbad_Mil_ControlTower.hpp" };
-	class Land_Mil_ControlTower: House_F { #include "Land_Jbad_Mil_ControlTower.hpp" };
-	class Land_Mil_Guardhouse_EP1: House_F { #include "Land_Jbad_Mil_Guardhouse.hpp" };
-	class Land_Mil_Guardhouse: House_F { #include "Land_Jbad_Mil_Guardhouse.hpp" };
-	class Land_Mil_House_EP1: House_F { #include "Land_Jbad_Mil_House.hpp" };
-	class Land_Mil_House: House_F { #include "Land_Jbad_Mil_House.hpp" };
+	class Land_Mil_Barracks_EP1: House_F {
+		#include "Land_Jbad_Mil_Barracks.hpp"
+	};
+	class Land_Mil_Barracks: House_F {
+		#include "Land_Jbad_Mil_Barracks.hpp"
+	};
+	class Land_Mil_ControlTower_EP1: House_F {
+		#include "Land_Jbad_Mil_ControlTower.hpp"
+	};
+	class Land_Mil_ControlTower: House_F {
+		#include "Land_Jbad_Mil_ControlTower.hpp"
+	};
+	class Land_Mil_Guardhouse_EP1: House_F {
+		#include "Land_Jbad_Mil_Guardhouse.hpp"
+	};
+	class Land_Mil_Guardhouse: House_F {
+		#include "Land_Jbad_Mil_Guardhouse.hpp"
+	};
+	class Land_Mil_House_EP1: House_F {
+		#include "Land_Jbad_Mil_House.hpp"
+	};
+	class Land_Mil_House: House_F {
+		#include "Land_Jbad_Mil_House.hpp"
+	};
 };
